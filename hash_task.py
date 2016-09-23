63663cd8292afc612b25fdfe375a0337a18822f3
LiubaKarlina <space_slona@mail.ru>
2016-09-23 13:49:28 +0300

    make diferent function from main

--- hash_task.py
+++ hash_task.py
@@ -2,33 +2,40 @@ import sys
 import os
 import hashlib
 
-def made_hash(filename):
+from collections import defaultdict
+
+dir = defaultdict(list)
+
+def make_hash(filename):
     size = 1024
     hash_md5 = hashlib.md5()
     with open(filename, "rb") as f:
         buf = f.read(size)
-        while len(buf) > 0:
+        while buf:
             hash_md5.update(buf)
             buf = f.read(size)
     return hash_md5.hexdigest()
 
-def main():
-    dir = {}
-    for path in sys.argv[1:]:
-       for root, dirs, files in os.walk(path):
-            for name in files:
-                if name[0] != "." and name[0] != "~":
-                  file_path = os.path.join(root, name)
-                  file_hash = made_hash(file_path)
-                  if file_hash not in dir:
-                    dir[file_hash] = []
-                  dir[file_hash].append(file_path)
+def show_dic():
     for v in dir.values():
         if len(v) > 1:
-            for file in v:
-              print(file,end = ":")
-            print("")
+            print(*v, sep = ":")
 
+
+def find_dubl(arg):
+    if len(sys.argv) != 2:
+        print('usage: ./d.py file')
+        sys.exit(1)
+    for path in sys.argv[1:]:
+        for root, _, files in os.walk(path):
+            for name in files:
+                if name[0] != "." and name[0] != "~":
+                    file_path = os.path.join(root, name)
+                    dir[make_hash(file_path)].append(file_path)
+
+def main():
+    find_dubl(sys.argv)
+    show_dic()
 
 if __name__ == "__main__":
     main()
