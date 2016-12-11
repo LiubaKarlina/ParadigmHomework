d4056e5dca4e15232e429f54de9b27e9f330c457
LiubaKarlina <space_slona@mail.ru>
2016-12-11 13:26:30 +0300

    b

--- SQL/4.sql
+++ SQL/4.sql
@@ -1,4 +1,4 @@
-select Country.Name, count(*) from 
-Country inner join City on Country.Code = City.CountryCode and City.population >= 1000000
-group by City.CountryCode
-order by count(*) desc, Country.Name;
+select Country.Name, count(distinct City.Id) from 
+Country left join City on Country.Code = City.CountryCode and City.Population >= 1000000
+group by Country.Code
+order by count(distinct City.Id) desc, Country.Name;
