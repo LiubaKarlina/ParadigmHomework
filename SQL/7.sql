b6d46c142d9ec2a872721ad925741640aba530b1
LiubaKarlina <space_slona@mail.ru>
2016-12-19 22:41:09 +0300

    all requests

--- SQL/7.sql
+++ SQL/7.sql
@@ -1,6 +1,6 @@
 select Country.Name from
-Country left join City on Country.Code = City.CountryCode and Country.Population > 0
+Country left join City on Country.Code = City.CountryCode
 group by Country.Code
-having Country.Population > 2 * sum(City.Population)
+having Country.Population > 2 * sum(City.Population) and Country.Population > 0
 order by Country.Name;
  
